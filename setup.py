461ed61ab03a254fa93e0e74687dfcebfe24e6fe
Michael Hao <mxhao2@illinois.edu>
2017-01-31 14:53:54 -0500

    fix yml

--- setup.py
+++ setup.py
@@ -1,13 +1,11 @@
 from setuptools import setup
 
-setup(name='sml',
+setup(name='helloTravis',
     version='0.1',
-    description='SQL like language for machine learning applications',
-    url='https://github.com/UI-DataScience/ML-SQL',
-    author='Neeraj Asthana, Michael Hao',
-    author_email='neeasthana@gmail.com, mike.hao@yahoo.com',
-    license='UIUC',
+    description='',
+    author='Michael Hao',
+    author_email='mike.hao@yahoo.com',
     install_requires=['markdown','pandas', 'pyparsing', 'requests', \
     'scikit-learn'],
-    packages=["sml",],
+    packages=["helloTravis",],
     zip_safe=False)
